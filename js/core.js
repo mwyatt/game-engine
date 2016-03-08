0b5a5ed06c904142fd72923e9fe0a3f38aa731c7
Martin Wyatt <martin.wyatt@gmail.com>
2016-03-08 17:30:07 +0000

    restructure vars, time for browserify? watch would be handy

--- js/core.js
+++ js/core.js
@@ -1,77 +1,75 @@
+var blocks = [];
+var keysDown = {};
+var timeThen = Date.now();
+var timeNow;
+var timeDelta;
+var timeModified;
+var canvasElement = document.createElement("canvas");
 
+canvasElement.width = 800;
+canvasElement.height = 600;
 
-// base object for things with
-// width
-// height
-// x
-// y
-var Hitbox = function() {
-  this.width;
-  this.height;
+var Stage = {
+  canvas: canvasElement,
+  ctx: canvasElement.getContext("2d")
+};
+
+// common object to extend from
+// global hitbox checks
+var Entity = function() {
+  this.w; // width
+  this.h; // height
   this.x;
   this.y;
 }
 
-Hitbox.prototype.getTop = function() {
+Entity.prototype.getTop = function() {
   return this.y;
 }
 
-Hitbox.prototype.getRight = function() {
-  return this.x + this.width;
+Entity.prototype.getRight = function() {
+  return this.x + this.w;
 }
 
-Hitbox.prototype.getBottom = function() {
-  return this.y + this.height;
+Entity.prototype.getBottom = function() {
+  return this.y + this.h;
 }
 
-Hitbox.prototype.getLeft = function() {
+Entity.prototype.getLeft = function() {
   return this.x;
 }
 
-// top 1
-// right 2
-// bottom 3
-// left 4
-// is this too much to test for each thing on each loop?
-// must ensure that only hitboxes are tested that are likely to be hit
-// may need detection to check when items are in certain areas
-Hitbox.prototype.isHit = function(entity) {
+Entity.prototype.isHit = function(entity) {
+  return this.getTop() > entity.getTop()
+    && this.getRight() < entity.getLeft()
+    && this.getLeft() < entity.getRight()
+    && this.getBottom < entity.getBottom();
+};
 
-  // top
-  if (entity.getBottom() >= this.getTop() && entity.getRight() >= this.getLeft() && entity.getLeft() <= this.getRight() && entity.getBottom() < this.getBottom()) {
-    return 1;
-
-  // right
-  } else if (this.getTop() >= entity.getBottom() && this.getRight() >= entity.getLeft() && this.getBottom() >= entity.getTop() && this.getLeft() < entity.getLeft()) {
-    return 2;
-
-  // bottom
-  } else if (this.getTop() < entity.getTop() && this.getRight() >= entity.getLeft() && this.getBottom() <= entity.getTop() && this.getLeft() >= entity.getLeft()) {
-    return 3;
-
-  // left
-  } else if (this.getTop() <= entity.getBottom() && this.getRight() > entity.getRight() && this.getBottom() >= entity.getTop() && this.getLeft() >= entity.getRight()) {
-    return 4;
-  }
+Entity.prototype.isHitTop = function(entity) {
+  return entity.getBottom() >= this.getTop() && entity.getRight() >= this.getLeft() && entity.getLeft() <= this.getRight() && entity.getBottom() < this.getBottom();
 }
 
-// Hitbox.prototype.isHitTop = function(entity) {
-//   return entity.getBottom() >= this.getTop()
-//     && entity.getTop() <= this.getBottom()
-//     && entity.getLeft() >= this.getLeft()
-//     && entity.getLeft() <= this.getRight();
-// }
-
-var EntityMoving = function() {
-  this.width;
-  this.height;
-  this.x;
-  this.y;
-  this.gravity;
-  this.speed;
-  this.vX;
-  this.vY;
+Entity.prototype.isHitRight = function(entity) {
+  return this.getTop() >= entity.getBottom() && this.getRight() >= entity.getLeft() && this.getBottom() >= entity.getTop() && this.getLeft() < entity.getLeft();
 }
+
+Entity.prototype.isHitBottom = function(entity) {
+  return this.getTop() < entity.getTop() && this.getRight() >= entity.getLeft() && this.getBottom() <= entity.getTop() && this.getLeft() >= entity.getLeft();
+}
+
+Entity.prototype.isHitLeft = function(entity) {
+  return this.getTop() <= entity.getBottom() && this.getRight() > entity.getRight() && this.getBottom() >= entity.getTop() && this.getLeft() >= entity.getRight();
+}
+
+// motion version of the entity
+var EntityMobile = function() {
+  this.g; // gravity
+  this.s; // speed
+  this.vX; // velocity
+  this.vY; // velocity
+}
+
 
 var Ball = function() {
   this.speed = 250;
@@ -81,40 +79,54 @@ var Ball = function() {
   this.vY = 5;
   this.vMax = 7;
   this.vDefault = 5;
-  this.width = 10;
-  this.height = 10;
+  this.w = 10;
+  this.h = 10;
   this.radius = 5;
   this.spin = 0;
   this.spinLife = 1;
 }
-Ball.prototype = Hitbox.prototype;
+Ball.prototype = Entity.prototype;
 
-Ball.prototype.moveV = function() {
+Ball.prototype.contactBlock = function() {
+  for (var i = blocks.length - 1; i >= 0; i--) {
+    if (i in blocks) {
+      if (hitLocation = blocks[i].isHit(ball)) {
 
-  // factor in spin
-  if (this.spin) {
-    // console.log(this.spin);
+        // 
+        if (hitLocation == 1 || hitLocation == 3) {
+          
+        };
+        delete blocks[i];
+      };
+    };
   };
+}
 
+Ball.prototype.contactPaddle = function() {
+  if (paddle.isHitTop(ball)) {
+
+    // ball go up
+    ball.vY = -ball.vY;
+
+    // ball always above paddle
+    ball.y -= paddle.height;
+
+    // load ball with spin if there
+    ball.spin = mouse.vX;
+  };
+}
+
+Ball.prototype.moveVelocity = function() {
+  timeModified
   this.x += this.vX;
   this.y += this.vY;
 }
 
-var Block = function() {
-  this.speed = 250;
-  this.x = 0;
-  this.y = 0;
-  this.width = 20;
-  this.height = 20;
-}
-Block.prototype = Hitbox.prototype;
-
-
-Ball.prototype.bounceCanvas = function(canvas) {
+Ball.prototype.bounceWalls = function(canvas) {
 
   // if this strikes the vertical walls, invert the 
   // x-velocity vector of this
-  if (this.x + this.width >= canvas.width) {
+  if (this.x + this.w >= canvas.width) {
     this.vX = -this.vX;
   } else if (this.x -this.radius <= 0) {
     this.vX = -this.vX;
@@ -122,7 +134,7 @@ Ball.prototype.bounceCanvas = function(canvas) {
 
   // if this strikes the horizontal walls, invert the 
   // x-velocity vector of this
-  if (this.y + this.width >= canvas.height) {
+  if (this.y + this.w >= canvas.height) {
     this.vY = -this.vY;
   } else if (this.y <= 0) {
     this.vY = -this.vY;
@@ -134,74 +146,78 @@ Ball.prototype.bounceCanvas = function(canvas) {
   } else if (this.x < 0) {
     this.x = 0;
   } else if (this.x > canvas.width) {
-    this.x = canvas.width - this.width;
+    this.x = canvas.width - this.w;
   } else if (this.y > canvas.height) {
-    this.y = canvas.height - this.height;
+    this.y = canvas.height - this.h;
   };
 }
 
-var Paddle = function() {
-  this.x = 0;
-  this.y = 0;
-  this.width = 150;
-  this.height = 10;
+var Block = function() {
+  this.w = 20;
+  this.h = 20;
 }
-Paddle.prototype = Hitbox.prototype;
+Block.prototype = Entity.prototype;
+
+var Paddle = function() {
+  this.w = 150;
+  this.h = 10;
+}
+Paddle.prototype = Entity.prototype;
+
+Paddle.prototype.mouseMove = function() {
+  if (mouse.x && mouse.y) {
+    paddle.x = mouse.x;
+    paddle.y = mouse.y;
+  };
+};
 
 var Mouse = function() {
   this.x = 0;
   this.y = 0;
-  this.speedX = 0;
-  this.prevX = 0;
+  this.vX = 0;
+  this.vXHistory = 0;
 }
 
-// game objects
+Mouse.prototype.storeVelocity = function() {
+  mouse.vX = mouse.vXHistory - mouse.x;
+  mouse.vXHistory = mouse.x;
+}
+
+for (var index = 30 - 1; index >= 0; index--) {
+  blocks[index] = new Block;
+  blocks[index].y = 20;
+  blocks[index].x = (30 * index);
+};
+
 var mouse = new Mouse;
+
 var ball = new Ball;
 ball.x = 100;
 ball.y = 100;
 
 var paddle = new Paddle;
 
-var blocks = [];
-for (var i = 30 - 1; i >= 0; i--) {
-  blocks[i] = new Block;
-  blocks[i].y = 20;
-  blocks[i].x = (30 * i);
-};
 
-// setup canvas and context
-var canvasElement = document.createElement("canvas");
-canvasElement.width = 800;
-canvasElement.height = 600;
-var stage = {
-  canvas: canvasElement,
-  ctx: canvasElement.getContext("2d")
-};
-
-// the loop game loop
-var timeThen = Date.now();
-var timeNow;
-var timeDelta;
-stage.loop = function () {
+Stage.loop = function () {
   timeNow = Date.now();
   timeDelta = timeNow - timeThen;
+  timeModified = timeDelta / 1000;
 
   // update positions of all things
-  stage.update(timeDelta / 1000);
+  Stage.update(timeModified);
 
   // draw objects
-  stage.render();
+  Stage.render();
 
   // store time to refer to at start
   timeThen = timeNow;
   
   // request to do this again asap
-  window.requestAnimationFrame(stage.loop);
+  window.requestAnimationFrame(Stage.loop);
 };
 
 // draw objects
-stage.render = function () {
+Stage.render = function () {
 
   // clear first
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -227,62 +243,14 @@ stage.render = function () {
   };
 };
 
-modifiers = 0;
-
 // update game objects
-stage.update = function (modifier) {
-
-  // ball moves its velocity
-  ball.moveV();
-
-  // ball bouncing off wall
-  ball.bounceCanvas(this.canvas);
-
-  // collision ball with paddle
-  // top
-  if (paddle.isHit(ball) == 1) {
-
-    // ball go up
-    ball.vY = -ball.vY;
-
-    // ball always above paddle
-    ball.y -= paddle.height;
-
-    // load ball with spin if there
-    ball.spin = mouse.speedX;
-  };
-
-  // mouse movement
-  if (mouse.x && mouse.y) {
-    paddle.x = mouse.x;
-    paddle.y = mouse.y;
-  };
-
-  // mouse speed
-  mouse.speedX = mouse.prevX - mouse.x;
-  mouse.prevX = mouse.x;
-
-  // ball hits block
-  for (var i = blocks.length - 1; i >= 0; i--) {
-    if (i in blocks) {
-      if (hitLocation = blocks[i].isHit(ball)) {
-
-        // 
-        if (hitLocation == 1 || hitLocation == 3) {
-          
-        };
-        delete blocks[i];
-      };
-    };
-  };
+Stage.update = function (modifier) {
+  ball.bounceWalls(this.canvas);
+  ball.contactPaddle();
+  ball.moveVelocity();
+  paddle.mouseMove();
+  mouse.storeVelocity();
 };
-
-
-/**
- * events
- */
-// store keys down in assoc array
-var keysDown = {};
 
 addEventListener("keydown", function (e) {
   keysDown[e.keyCode] = true;
@@ -293,14 +261,14 @@ addEventListener("keyup", function (e) {
 }, false);
 
 // store mouse pos
-stage.canvas.addEventListener('mousemove', function(e) {
-  var rect = stage.canvas.getBoundingClientRect();
+Stage.canvas.addEventListener('mousemove', function(e) {
+  var rect = Stage.canvas.getBoundingClientRect();
   mouse.x = e.clientX - rect.left;
   mouse.y = e.clientY - rect.top;
 }, false);
 
 // append canvas to dom
-document.body.appendChild(stage.canvas);
+document.body.appendChild(Stage.canvas);
 
 // let's play this game!
-stage.loop();
+Stage.loop();
