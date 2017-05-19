027435d06eef0ed781ca9290a48cf9c92d604707
Martin Wyatt <martin.wyatt@gmail.com>
2017-05-19 17:00:12 +0100

    particle experiment init

---
+++ index-particle.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Particle playground</title>
+</head>
+<body>
+    <script src="ui/particle.bundle.js?<?php echo filemtime('ui/particle.bundle.js') ?>"></script>
+</body>
+</html>
